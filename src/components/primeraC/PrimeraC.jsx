a56cc4247ba1fc7e9a5d77590d653f9544acf8aa
eloy <eloysandoval10@hotmail.com>
2023-08-25 12:28:06 -0500

    actualizacion de la tabla

--- src/components/primeraC/PrimeraC.jsx
+++ src/components/primeraC/PrimeraC.jsx
@@ -29,11 +29,11 @@ const PrimeraC = () => {
       <div className="container_div">
         <div className="container_div1">
           <h6 className="h6_pos">1</h6>
-          <h6>Equipo Azul</h6>
+          <h6>FC Internacional</h6>
         </div>
         <div className="container_div1">
-          <h6 className="h6_pos">31</h6>
-          <h6>12</h6>
+          <h6 className="h6_pos">39</h6>
+          <h6>17</h6>
         </div>
       </div>
       {/* // 2 */}
@@ -43,63 +43,63 @@ const PrimeraC = () => {
           <h6>Napoles</h6>
         </div>
         <div className="container_div1">
-          <h6 className="h6_pos">26</h6>
-          <h6>12</h6>
+          <h6 className="h6_pos">37</h6>
+          <h6>17</h6>
         </div>
       </div>
       {/* // 3 */}
       <div className="container_div">
         <div className="container_div1">
           <h6 className="h6_pos">3</h6>
-          <h6>FC Internacional</h6>
+          <h6> DEPORTIVO J.M.</h6>
         </div>
         <div className="container_div1">
-          <h6 className="h6_pos">24</h6>
-          <h6>12</h6>
+          <h6 className="h6_pos">36</h6>
+          <h6>17</h6>
         </div>
       </div>
       {/* // 4 */}
       <div className="container_div">
         <div className="container_div1">
           <h6 className="h6_pos">4</h6>
-          <h6> DEPORTIVO J.M.</h6>
+          <h6>Sampdoria Becerril</h6>
         </div>
         <div className="container_div1">
-          <h6 className="h6_pos">24</h6>
-          <h6>12</h6>
+          <h6 className="h6_pos">35</h6>
+          <h6>17</h6>
         </div>
       </div>
       {/* // 5 */}
       <div className="container_div">
-        <div className="container_div1">
+        <div className="container_div1">    
           <h6 className="h6_pos">5</h6>
-          <h6>Sampdoria Becerril</h6>
+          <h6>EQUIPO AZUL FC</h6>
         </div>
         <div className="container_div1">
-          <h6 className="h6_pos">17</h6>
-          <h6>12</h6>
+          <h6 className="h6_pos">31</h6>
+          <h6>16</h6>
         </div>
       </div>
       {/* // 6 */}
       <div className="container_div">
         <div className="container_div1">
           <h6 className="h6_pos">6</h6>
-          <h6>San Francisco</h6>
+          <h6>Sultanes F G.</h6>
         </div>
         <div className="container_div1">
-          <h6 className="h6_pos">16</h6>
-          <h6>12</h6>
+          <h6 className="h6_pos">22</h6>
+          <h6>17</h6>
         </div>
       </div>
       {/* // 7 */}
       <div className="container_div">
         <div className="container_div1">
           <h6 className="h6_pos">7</h6>
-          <h6>Sultanes F G.</h6>
+          <h6>San Francisco</h6>
         </div>
         <div className="container_div1">
-          <h6 className="h6_pos">15</h6>
-          <h6>12</h6>
+          <h6 className="h6_pos">19</h6>
+          <h6>16</h6>
         </div>
       </div>
       {/* // 8 */}
@@ -110,7 +110,7 @@ const PrimeraC = () => {
         </div>
         <div className="container_div1">
           <h6 className="h6_pos">14</h6>
-          <h6>12</h6>
+          <h6>16</h6>
         </div>
       </div>
       {/* // 9 */}
@@ -121,7 +121,7 @@ const PrimeraC = () => {
         </div>
         <div className="container_div1">
           <h6 className="h6_pos">0</h6>
-          <h6>7</h6>
+          <h6>14</h6>
         </div>
       </div>
       {/* 10 */}
@@ -132,17 +132,17 @@ const PrimeraC = () => {
         </div>
         <div className="container_div1">
           <h6 className="h6_pos">0</h6>
-          <h6>7</h6>
+          <h6>13</h6>
         </div>
       </div>
 
       <div className="container_iframe_primerac">
         <iframe
-          src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F100093630360338%2Fvideos%2F3079031232230813%2F&show_text=false&width=560&t=0"
+          src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F100093630360338%2Fvideos%2F1428492264617958%2F&show_text=false&width=560&t=0"
           className="iframe_primeraC"
         ></iframe>
         <iframe
-          src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F100093630360338%2Fvideos%2F1718081678633481%2F&show_text=false&width=560&t=0"
+          src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F100093630360338%2Fvideos%2F238610585788741%2F&show_text=false&width=560&t=0"
           className="iframe_primeraC"
         ></iframe>
       </div>
